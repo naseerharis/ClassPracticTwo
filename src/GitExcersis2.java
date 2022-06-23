3903a322f8f76fa6b0f140a60cb2a2a38107bbde
naseerharis <naseer.haris@tekschool.us>
2022-06-23 10:23:42 -0700

    new print

--- src/GitExcersis2.java
+++ src/GitExcersis2.java
@@ -1,7 +1,14 @@
 
 public class GitExcersis2 {
+	public static void main(String[] args){
+		int sum = 0;
+		 System.out.println(sum);
+	}
   public int sum (int a , int b){
 	  int sum = a+b;
 	  return sum;
+	 
   }
+ 
+  
 }
