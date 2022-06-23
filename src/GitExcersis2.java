b17083cd267e80c0cc1dc5c67b651fdd4cb06bd8
naseerharis <naseer.haris@tekschool.us>
2022-06-23 09:38:04 -0700

    new div method

--- src/GitExcersis2.java
+++ src/GitExcersis2.java
@@ -1,7 +1,17 @@
 
 public class GitExcersis2 {
   public int sum (int a , int b){
-	  int sum = a+b;
+	  int sum = a-b;
 	  return sum;
   }
+  public int mult(int a,int b){
+	  int mult = a*b;
+	  return mult;
+	  
+  }
+  public int div(int a,int b){
+	  int div = a/b;
+	  return div;
+  }
 }
+
